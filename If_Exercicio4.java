e854a53bf020359dbfa062667904ebe3d426ea4e
Luis Sibim <141850109+lfsibim@users.noreply.github.com>
2023-11-10 09:46:12 -0300

    Alteração if exercicio 4

--- If_Exercicio4.java
+++ If_Exercicio4.java
@@ -13,53 +13,49 @@ public class If_Exercicio4 {
 		String carac1, carac2, carac3;
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("\nDigite a primeira característica: ");
+		System.out.println("Vertebrado ou Invertebrado?");
 		carac1 = input.nextLine();
-		System.out.println("\nDigite a segunda característica: ");
-		carac2 = input.nextLine();
-		System.out.println("\nDigite a terceira característica: ");
-		carac3 = input.nextLine();
 		
 		if(carac1.equalsIgnoreCase("vertebrado")) {
+			System.out.println("\nAve ou Mamífero?");
+			carac2 = input.nextLine();
 			if(carac2.equalsIgnoreCase("ave")) {
+				System.out.println("\nCarnívoro ou Onívoro?");
+				carac3 = input.nextLine();
 				if(carac3.equalsIgnoreCase("carnívoro")) {
-					System.out.println("\nÉ uma Águia");
+					System.out.println("\nÉ uma Águia!");
 				}else {
-					System.out.println("\nÉ uma Pomba");
+					System.out.println("\nÉ uma Pomba!");
+				}
+			}else {
+				System.out.println("\nOnívoro ou Herbívoro?");
+				carac3 = input.nextLine();
+				if(carac3.equalsIgnoreCase("Onívoro")) {
+					System.out.println("\nÉ um Homem!");
+				}else {
+					System.out.println("\nÉ uma Vaca!");
 				}
 			}
-		}
-		
-		if(carac1.equalsIgnoreCase("vertebrado")) {
-			if(carac2.equalsIgnoreCase("mamífero")) {
-				if(carac3.equalsIgnoreCase("onívoro")) {
-					System.out.println("\nÉ um Homem");
-				}else {
-					System.out.println("\nÉ uma Vaca");
-				}
-			}
-		}
-		
-		if(carac1.equalsIgnoreCase("invertebrado")) {
+		}else {
+			System.out.println("\nInseto ou Anelídeo?");
+			carac2 = input.nextLine();
 			if(carac2.equalsIgnoreCase("inseto")) {
-				if(carac3.equalsIgnoreCase("hematófago")) {
-					System.out.println("\nÉ uma Pulga");
+				System.out.println("\nHematófago ou Herbívoro");
+				carac3 = input.nextLine();
+				if(carac3.equalsIgnoreCase("Hematófago")) {
+					System.out.println("\nÉ uma Pulga!");
 				}else {
-					System.out.println("\nÉ uma Lagarta");
+					System.out.println("\nÉ uma Lagarta!");
+				}
+			}else {
+				System.out.println("\nHematófago ou Onívoro");
+				carac3 = input.nextLine();
+				if(carac3.equalsIgnoreCase("hematófago")) {
+					System.out.println("\nÉ um Sanguessuga!");
+				}else {
+					System.out.println("\nÉ uma Minhoca!");
 				}
 			}
 		}
-		
-		if(carac1.equalsIgnoreCase("invertebrado")) {
-			if(carac2.equalsIgnoreCase("anelídeo")) {
-				if(carac3.equalsIgnoreCase("hematófago")) {
-					System.out.println("\nÉ um Sanguessuga");
-				}else {
-					System.out.println("\nÉ uma Minhoca");
-				}
-			}
-		}
-
 	}
-
 }
